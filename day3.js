dd35332285e41080271916a3a041f8200724c333
mmurad6074-arch <mmurad6074@gmail.com>
2026-08-05 13:06:50 +0500

    Added day3.js

--- day3.js
+++ day3.js
@@ -1,1 +1,7 @@
-console.log("day3");
+let name = "Murad";
+let age = 22;
+let city = "Lahore";
+
+console.log("My Name is:", name);
+console.log("My Age is:", age);
+console.log("I Live in:", city);
